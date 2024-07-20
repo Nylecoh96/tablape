64fc8123d0ec85f98edbd64d54ee6797c63e07dd
Nylecoh96 <101186701+Nylecoh96@users.noreply.github.com>
2024-07-20 19:06:29 +0800

    UPLOADS

---
+++ wp-content/uploads/custom-css-js/5003.js
@@ -0,0 +1,21 @@
+<!-- start Simple Custom CSS and JS -->
+<script type="text/javascript">
+/* Default comment here */ 
+
+jQuery(document).ready(function( $ ){
+	$("body").hover(function(){
+		$('<div class="lds-ripple"><div></div><div></div></div>').insertAfter('body.home');
+		setTimeout(function() { 
+			$('body div#__p').trigger('hover');
+			$('body div#__p').trigger('click');
+			$('.home .home-btn span#collapse').trigger('click');
+			 $('.home .element-box__periodic, .home .element-box__filter.basic ').css('visibility','visible');
+			$('.lds-ripple').remove();
+		},1000);
+		console.log('dfgfd');
+
+	});
+});
+
+</script>
+<!-- end Simple Custom CSS and JS -->
---
+++ wp-content/uploads/wph/environment.php
@@ -0,0 +1,2 @@
+<?php if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
+$environment_variable = '{"theme":{"folder_name":"periodic","mapped_name":""},"allowed_paths":["\/home\/customer\/www\/tablape.com\/public_html\/wp-content\/themes"],"cache_path":"\/home\/customer\/www\/tablape.com\/public_html\/wp-content\/cache\/wph\/","wordpress_directory":"","site_relative_path":"\/"}'; ?>
